9b37ff554c051cf85f642deddcb4c35a748df718
Joe Quattrone <joequattrone5@gmail.com>
2018-10-29 15:43:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli)
-  // katz_deli is no longer an array of names but an array of ticket numbers
+  #katz_deli is no longer an array of names but an array of ticket numbers
   katz_deli.push(name)
   puts ("Welcome, your ticket number is #{katz_deli}. You are number #{katz_deli.length} in line.")
 
