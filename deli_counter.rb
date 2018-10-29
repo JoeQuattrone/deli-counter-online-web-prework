0438979d4128ce5d421dbbf066bac0a3d32ef96d
Joe Quattrone <joequattrone5@gmail.com>
2018-10-29 15:50:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,12 +18,12 @@ end
 def take_a_number(katz_deli)
   #katz_deli is no longer an array of names but an array of ticket numbers
   #1. push the number 1 to the katz_deli array
-  
-  katz_deli.push(katz_deli.length + 1)
+  ticket = katz_deli.length + 1
+  katz_deli.push(ticket)
   #2 output the ticket number and the place in line to the user
 
   
-  puts ("Welcome, your ticket number is #{begining}. You are number #{katz_deli.length} in line.")
+  puts ("Welcome, your ticket number is #{ticket}. You are number #{katz_deli.length} in line.")
  
 end
 
